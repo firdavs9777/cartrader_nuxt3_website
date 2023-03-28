180b095846ec6923e7daf45c822db7c8f544b905
firdavs9777 <fmutalipov97@gmail.com>
2023-03-28 22:16:34 +0900

    First Commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+ <!-- Navbar-->
+ <header class="sticky top-0 z-50 
+ flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+  <a href="" class="bold text-2xl font-mono text-black">Quintet CarTrader Corporation</a>
+ </header>
+ <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('/assets/ferrari.jpeg')]">
+ </div>
+  </div>
+</template>
